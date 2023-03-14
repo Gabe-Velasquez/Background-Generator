251fe6bd27f48ad82421f8c39fcaf5a7b89e328b
Gabe <gabriel.velasquez0116@gmail.com>
2023-03-14 10:34:56 -0600

    copy button feature working now

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 var randomBtn = document.getElementById("random");
+var copy = document.querySelector("#copyBtn");
 
 //creates function to change background color value
 function setGradient() {
@@ -21,24 +22,27 @@ window.onload = setGradient();
 
 //random value function
 function getRandomGradient(){
-    var randomColor1= "#" + Math.floor((Math.random()*)(1<<24|0)).toString(16);
-    var randomColor2= "#" + Math.floor((Math.random()*)(1<<24|0)).toString(16);
+    var randomColor1 = '#'+ Math.floor((Math.random()*(1<<24)|0)).toString(16);
+	var randomColor2 = '#'+ Math.floor((Math.random()*(1<<24)|0)).toString(16);
 
     color1.value = randomColor1;
     color2.value = randomColor2;
 
     setGradient();
 	css.textContent = `linear-gradient(to right, 
-        ${color1.value}, ${color2.value});`.toUpperCase();
+    ${color1.value}, ${color2.value});`.toUpperCase();
 }
 
 //copy button function
-function copyText(){
-    var copyText = css;
-    copyText.select();
-    document.execCommand("copy");
-}
-document.querySelector("#copyBtn").addEventListener("click", copyText);
+copy.addEventListener("click", () => {
+    navigator.clipboard.writeText(css.textContent)
+    .then(()=> {
+        console.log('text copied');
+    })
+    .catch((err) => {
+        console.err('Failed Copy: ', err);
+    });
+});
 
 //adds any changes to color selectors and runs function through them
 color1.addEventListener("input", setGradient);
